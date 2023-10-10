2d81ef6f8d329bbf00e46823a2434c7b27727935
harish-hyperDev <coolprogrammer.777@gmail.com>
2023-10-10 19:26:01 +0530

    code cleanup

--- server/tests/test_fetch_user.py
+++
@@ -1,8 +0,0 @@
-from actions.UserActions import UserActions
-from models.model import UserModel
-
-def test_fetch():
-    result = UserActions.get(email="test@aloha.com")
-    
-    print(result)
-    # assert result == UserResponse.CREATED
